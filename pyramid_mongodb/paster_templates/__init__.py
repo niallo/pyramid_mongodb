34b997571d658704f255427d9682b1dd5682a300
Michael A. Smith <smithm@trueaction.com>
2012-04-06 17:31:03 -0400

    Follow Pyramid directions for supporting multiple versions.
    http://docs.pylonsproject.org/projects/pyramid/en/1.3-branch/narr/scaffolding.html#supporting-older-pyramid-versions

--- pyramid_mongodb/paster_templates/__init__.py
+++ pyramid_mongodb/paster_templates/__init__.py
@@ -1,27 +1,17 @@
-import os
-
-from paste.script.templates import Template
-from paste.util.template import paste_script_template_renderer
-
-class PyramidTemplate(Template):
-    def pre(self, command, output_dir, vars):
-        vars['random_string'] = os.urandom(20).encode('hex')
-        package_logger = vars['package']
-        if package_logger == 'root':
-            # Rename the app logger in the rare case a project is named 'root'
-            package_logger = 'app'
-        vars['package_logger'] = package_logger
-        return Template.pre(self, command, output_dir, vars)
-
-    def post(self, command, output_dir, vars):
-        self.out('Welcome to Pyramid.  Sorry for the convenience.')
-        return Template.post(self, command, output_dir, vars)
-
-    def out(self, msg): # pragma: no cover (replaceable testing hook)
-        print msg
+try: # pyramid 1.0.X
+    # "pyramid.paster.paste_script..." doesn't exist past 1.0.X
+    from pyramid.paster import paste_script_template_renderer
+    from pyramid.paster import PyramidTemplate
+except ImportError:
+    try: # pyramid 1.1.X, 1.2.X
+        # trying to import "paste_script_template_renderer" fails on 1.3.X
+        from pyramid.scaffolds import paste_script_template_renderer
+        from pyramid.scaffolds import PyramidTemplate
+    except ImportError: # pyramid >=1.3a2
+        paste_script_template_renderer = None
+        from pyramid.scaffolds import PyramidTemplate
 
 class MongodbProjectTemplate(PyramidTemplate):
     _template_dir = 'pyramid_mongodb'
     summary = 'pyramid MongoDB project'
     template_renderer = staticmethod(paste_script_template_renderer)
-
